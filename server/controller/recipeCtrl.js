acb60e6d18a5ab9ce6bedf9d1bf7431881696f71
sourabhpande532 <sourabhpande43@gmail.com>
2026-03-30 21:53:31 +0530

    Add: recipe list add using api's

--- server/controller/recipeCtrl.js
+++ server/controller/recipeCtrl.js
@@ -1,9 +1,45 @@
+const Recipe = require("../model/Recipe");
 exports.obtainedAllRecipe = async (req, res) => {
   try {
-  } catch (error) {}
+    const recipe = await Recipe.find();
+    if (recipe.length != 0) {
+      res.status(200).json({
+        success: true,
+        message: "Fetched Recipe",
+        data: { recipe },
+      });
+    } else {
+      res.status(404).json({ success: true, message: "Job not found." });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      error: error,
+      message: "Failed to fetch recipe error",
+    });
+  }
 };
 
 exports.addRecipeToDatabase = async (req, res) => {
   try {
-  } catch (error) {}
+    const { name, cuisine, ingredients, instructions, images } = req.body;
+    if (!name || !cuisine || !ingredients || !instructions || !images) {
+      return res
+        .status(400)
+        .json({ success: false, message: "required missing fields" });
+    }
+    const newRecipe = new Recipe(req.body);
+    await newRecipe.save();
+    return res.status(201).json({
+      success: true,
+      message: "Added Successfully",
+      data: { recipe: newRecipe },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, error: error, message: "Server create error" });
+    console.error(error);
+  }
 };
--- server/model/Recipe.js
+++ server/model/Recipe.js
@@ -1,3 +1,30 @@
 const mongoose = require("mongoose");
-const RecipeSchema = new mongoose({});
+const RecipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  ingredients: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  instructions: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  images: [{ type: String }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 module.exports = mongoose.model("Recipe", RecipeSchema);
